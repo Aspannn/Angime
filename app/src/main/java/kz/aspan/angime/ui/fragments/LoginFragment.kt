2f0a80dafe494199beb9338a4d07b9e2e52ac398
aspan <aspansdu@gmail.com>
2021-05-13 16:38:27 +0600

    Firebase, Hilt  Setup

---
+++ app/src/main/java/kz/aspan/angime/ui/fragments/LoginFragment.kt
@@ -0,0 +1,16 @@
+package kz.aspan.angime.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kz.aspan.angime.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
