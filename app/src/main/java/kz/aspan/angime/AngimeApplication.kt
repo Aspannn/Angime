7fdaaa3a6dd4eb0b380a02e3a1114e780d812980
aspan <aspansdu@gmail.com>
2021-05-13 17:51:14 +0600

    hilt setup

---
+++ app/src/main/java/kz/aspan/angime/AngimeApplication.kt
@@ -0,0 +1,8 @@
+package kz.aspan.angime
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AngimeApplication : Application() {
+}
---
+++ app/src/main/java/kz/aspan/angime/di/AppModule.kt
@@ -0,0 +1,35 @@
+package kz.aspan.angime.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kz.aspan.angime.R
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplicationContext(
+        @ApplicationContext context: Context
+    ) = context
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_error)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
